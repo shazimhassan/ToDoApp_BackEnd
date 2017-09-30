1f48bb67da48c23c1d11211e6c9418e98d3846f5
Shazim Hassan <shazim.hassan2000@gmail.com>
2017-09-30 19:08:13 +0000

    hyperlink and referenced show links (todo_controller and routes.rb)

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -1,9 +1,29 @@
 class TodoController < ApplicationController
   def index
   end
-  
   def show
-    @todo_description = "Make the curriculum"
-    @todo_pomodoro_estimate = 4
+    todo_id = params[:id]
+    if (todo_id == "1") 
+      @description = 'Make the curriculum'
+      @estimate = 4
+    elsif (todo_id == "2") 
+      @description = 'Buy workshop supplies'
+      @estimate = 3
+    elsif (todo_id == "3") 
+      @description = 'Meet with the volunteer trainers'
+      @estimate = 2
+    elsif (todo_id == "4") 
+      @description = 'Order food for Saturday and Sunday'
+      @estimate = 1
+    elsif (todo_id == "5") 
+      @description = 'Check pre-work assignments'
+      @estimate = 2
+    elsif (todo_id == "6") 
+      @description = 'Send workshop location to all students'
+      @estimate = 1
+    elsif (todo_id == "7") 
+      @description = 'Have a great workshop'
+      @estimate = 0
+    end
   end
 end
