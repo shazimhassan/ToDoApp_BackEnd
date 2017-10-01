f5bf9fa4f37b6c77950f14d53e1ab15d8c6e2cdc
Shazim Hassan <shazim.hassan2000@gmail.com>
2017-10-01 16:03:22 +0000

    Added RCAV (Ruby) functionality to the webpage (new and create)

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
  get 'todo/index', to: 'todo#index'
  get 'todo/show/:id', to: 'todo#show'
  get 'todo/new', to: 'todo#new'
- get 'todo/create', to: 'todo#create'
+ post 'todo/new', to: 'todo#create'
 end
