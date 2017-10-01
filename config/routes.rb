9d36a0145457c2030667c5c9cbff26a65b72673b
Shazim Hassan <shazim.hassan2000@gmail.com>
2017-10-01 18:06:59 +0000

    Added editing and updating To-Do list capability

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
  get 'todo/show/:id', to: 'todo#show'
  get 'todo/new', to: 'todo#new'
  post 'todo/new', to: 'todo#create'
+ get 'todo/edit/:id', to: 'todo#edit'
+ put 'todo/edit/:id', to: 'todo#update'
+ put 'todo/edit/:id/destroy', to: 'todo#destroy'
 end
